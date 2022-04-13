888fac5f347cdbd3b5bd5ecc99c614370e551a70
panliu0310 <liuhoipan@gmail.com>
2022-04-13 21:19:48 +0800

    Initial commit

---
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.cuhk.csci3310.quizdle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
