0faf419e8576fc96f2ad101af8e25128220c94fb
panliu0310 <liuhoipan@gmail.com>
2022-04-16 02:02:20 +0800

    Add bottom navigation bar

--- app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
@@ -1,14 +1,53 @@
 package edu.cuhk.csci3310.quizdle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setSelectedItemId(0);
     }
+
+    HomeFragment homeFragment = new HomeFragment();
+    LevelFragment levelFragment = new LevelFragment();
+    BattleFragment battleFragment = new BattleFragment();
+    DiscussFragment discussFragment = new DiscussFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case 0:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                return true;
+
+            case 1:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, levelFragment).commit();
+                return true;
+
+            case 2:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, battleFragment).commit();
+                return true;
+
+            case 3:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, discussFragment).commit();
+                return true;
+        }
+        return false;
+    }
+
 }
