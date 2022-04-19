6fe509268b8b590f28aee314fb2f2260d62703c1
panliu0310 <liuhoipan@gmail.com>
2022-04-19 21:29:09 +0800

    Fixed bottom navigation fragment display

--- app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
@@ -3,7 +3,9 @@ package edu.cuhk.csci3310.quizdle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -16,15 +18,16 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (linkedAccount) {
             setContentView(R.layout.activity_main);
-
             bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-            bottomNavigationView.setSelectedItemId(0);
+            bottomNavigationView.setOnNavigationItemSelectedListener(this);
+            bottomNavigationView.setSelectedItemId(R.id.home);
         }else{
             setContentView(R.layout.activity_login);
         }
+
     }
 
     HomeFragment homeFragment = new HomeFragment();
@@ -32,24 +35,25 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     BattleFragment battleFragment = new BattleFragment();
     DiscussFragment discussFragment = new DiscussFragment();
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
+        Log.d("item.getItemId", String.valueOf(item.getItemId()));
         switch (item.getItemId()) {
-            case 0:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
                 return true;
 
-            case 1:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, levelFragment).commit();
+            case R.id.level:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, levelFragment).commit();
                 return true;
 
-            case 2:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, battleFragment).commit();
+            case R.id.battle:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, battleFragment).commit();
                 return true;
 
-            case 3:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, discussFragment).commit();
+            case R.id.discuss:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, discussFragment).commit();
                 return true;
         }
         return false;
