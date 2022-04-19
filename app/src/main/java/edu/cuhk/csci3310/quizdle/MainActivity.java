cce2aa71770c0d0a613ef4bf53179384d0aa0b01
hea0516 <hea4152@gmail.com>
2022-04-19 20:08:57 +0800

    Connected to firebase

--- app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
@@ -11,15 +11,20 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
+    private boolean linkedAccount = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (linkedAccount) {
+            setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+            bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        bottomNavigationView.setSelectedItemId(0);
+            bottomNavigationView.setSelectedItemId(0);
+        }else{
+            setContentView(R.layout.activity_login);
+        }
     }
 
     HomeFragment homeFragment = new HomeFragment();
