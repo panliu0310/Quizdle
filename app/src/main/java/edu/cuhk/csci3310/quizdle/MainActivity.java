f994ac0ad38e44f8d7b3d87897e28565fdd7b55a
hea0516 <hea4152@gmail.com>
2022-04-21 22:20:35 +0800

    trying authentication

--- app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
@@ -1,36 +1,71 @@
 package edu.cuhk.csci3310.quizdle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Collections;
+
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    private boolean noAccount = true;
+    private FirebaseAuth mAuth;
+    private String TAG = "HomePage";
+
+
     BottomNavigationView bottomNavigationView;
-    private boolean linkedAccount = false;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (linkedAccount) {
-            setContentView(R.layout.activity_main);
-
-            bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-            bottomNavigationView.setSelectedItemId(0);
-        }else{
-            setContentView(R.layout.activity_login);
-        }
-    }
 
     HomeFragment homeFragment = new HomeFragment();
     LevelFragment levelFragment = new LevelFragment();
     BattleFragment battleFragment = new BattleFragment();
     DiscussFragment discussFragment = new DiscussFragment();
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setSelectedItemId(0);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (noAccount){
+            starttSignIn();
+        }
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -53,6 +88,40 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 return true;
         }
         return false;
+    }
+
+    private void starttSignIn(){
+        // Choose authentication providers
+        /*List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.TwitterBuilder().build());*/
+        AuthUI.IdpConfig provider = new AuthUI.IdpConfig.GoogleBuilder().build();
+
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Collections.singletonList(provider))
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Log.d(TAG, String.valueOf(user));
+            // ...
+        } else {
+            Log.d(TAG, "Sign in failed");
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
     }
 
 }
