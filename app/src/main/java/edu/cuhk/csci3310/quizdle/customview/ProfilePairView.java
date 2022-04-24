5e7b0ddf7f8ddad8d309d0c406fb977e7d688aba
panliu0310 <liuhoipan@gmail.com>
2022-04-24 18:56:08 +0800

    create custom view ProfilePairView

---
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/customview/ProfilePairView.java
@@ -0,0 +1,49 @@
+package edu.cuhk.csci3310.quizdle.customview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import edu.cuhk.csci3310.quizdle.R;
+
+public class ProfilePairView extends ConstraintLayout {
+
+    TextView tvName;
+    TextView tvContent;
+    private final String name;
+    private String content;
+
+    public ProfilePairView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        View.inflate(context,R.layout.view_profile_pair,this);
+        tvName = findViewById(R.id.tv_name);
+        tvContent = findViewById(R.id.tv_content);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.ProfilePairView,
+                0, 0);
+        try {
+            name = a.getString(R.styleable.ProfilePairView_profileName);
+        } finally {
+            a.recycle();
+        }
+        fetchProfileFromFirestore();
+        setField();
+    }
+
+    private void setField(){
+        tvName.setText(name);
+        tvContent.setText(content);
+    }
+
+    public void fetchProfileFromFirestore(){
+        content = "testing";
+    }
+}
