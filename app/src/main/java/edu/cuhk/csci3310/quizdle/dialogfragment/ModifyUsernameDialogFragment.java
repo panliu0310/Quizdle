19bc4127ccaf3fd95580323930a0e4a63e41489c
panliu0310 <liuhoipan@gmail.com>
2022-05-04 22:58:05 +0800

    Delete unused import

--- app/src/main/java/edu/cuhk/csci3310/quizdle/dialogfragment/ModifyUsernameDialogFragment.java
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/dialogfragment/ModifyUsernameDialogFragment.java
@@ -1,6 +1,5 @@
 package edu.cuhk.csci3310.quizdle.dialogfragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,34 +7,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.WriteBatch;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
-import edu.cuhk.csci3310.quizdle.MainActivity;
 import edu.cuhk.csci3310.quizdle.R;
-import edu.cuhk.csci3310.quizdle.SettingActivity;
 
 public class ModifyUsernameDialogFragment extends DialogFragment {
 
