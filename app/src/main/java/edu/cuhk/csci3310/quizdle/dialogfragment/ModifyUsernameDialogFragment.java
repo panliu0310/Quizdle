4587a2963bbb86b040ff32bec50bb33af54f9458
panliu0310 <liuhoipan@gmail.com>
2022-05-04 22:54:18 +0800

    Successfully update username

--- app/src/main/java/edu/cuhk/csci3310/quizdle/dialogfragment/ModifyUsernameDialogFragment.java
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/dialogfragment/ModifyUsernameDialogFragment.java
@@ -1,20 +1,51 @@
 package edu.cuhk.csci3310.quizdle.dialogfragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.WriteBatch;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import edu.cuhk.csci3310.quizdle.MainActivity;
 import edu.cuhk.csci3310.quizdle.R;
+import edu.cuhk.csci3310.quizdle.SettingActivity;
 
 public class ModifyUsernameDialogFragment extends DialogFragment {
 
+    String TAG = "ModifyUsernameDialogFragment";
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseFirestore mFirestore;
+
     String username;
+    String email;
 
     public static ModifyUsernameDialogFragment newInstance(String username) {
         ModifyUsernameDialogFragment fragment = new ModifyUsernameDialogFragment();
@@ -31,6 +62,16 @@ public class ModifyUsernameDialogFragment extends DialogFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         username = getArguments().getString("username");
+
+        // initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        // get current user
+        FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+        email = firebaseUser.getEmail();
+
+        // Enable Firestore logging
+        FirebaseFirestore.setLoggingEnabled(true);
+        mFirestore = FirebaseFirestore.getInstance();
     }
 
     @Override
@@ -39,6 +80,74 @@ public class ModifyUsernameDialogFragment extends DialogFragment {
         View view = inflater.inflate(R.layout.fragment_modify_username_dialog, container, false);
         EditText etUsername = view.findViewById(R.id.et_username);
         etUsername.setText(username);
+        Button btnOk = view.findViewById(R.id.btn_ok);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateUsername(String.valueOf(etUsername.getText()));
+            }
+        });
         return view;
+    }
+
+    private void validateUsername(String username){
+        // before modify username, first check validity
+        if (username.length() <= 5 || username.length() >= 31) {
+            // check username length
+            Log.d(TAG, "username length invalid");
+            this.dismiss();
+        }
+
+        // check existence of the username in database
+        CollectionReference usersRef = mFirestore.collection("users");
+        usersRef.whereEqualTo("username", username)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "task success");
+                            if (task.getResult().isEmpty()){
+                                // no such username, so the user can use this username
+                                Log.d(TAG, "no such username exist, the username can be used");
+                                updateUsername(username);
+                            } else {
+                                Log.d(TAG, "username exist");
+                                dismiss();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void updateUsername(String username){
+        // create a HashMap to store username
+        Map<String, Object> userDetail = new HashMap<>();
+        userDetail.put("username", username);
+
+        CollectionReference usersRef = mFirestore.collection("users");
+        usersRef.whereEqualTo("email", email)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                            String documentId = documentSnapshot.getId();
+                            mFirestore.collection("users")
+                                    .document(documentId)
+                                    .update(userDetail)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            Log.d(TAG, "successfully update username");
+                                            dismiss();
+                                        }
+                                    });
+                        }
+                    }
+                });
+
+
     }
 }
