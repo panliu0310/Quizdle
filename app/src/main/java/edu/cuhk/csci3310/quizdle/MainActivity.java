34ad03050fc5312b04380140f733063611419bc2
panliu0310 <liuhoipan@gmail.com>
2022-05-06 23:36:40 +0800

    Create Question system

--- app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     BottomNavigationView bottomNavigationView;
 
     HomeFragment homeFragment = new HomeFragment();
+    CreateFragment createFragment = new CreateFragment();
     SinglePlayerFragment singlePlayerFragment = new SinglePlayerFragment();
     BattleFragment battleFragment = new BattleFragment();
     DiscussFragment discussFragment = new DiscussFragment();
@@ -103,6 +104,10 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         switch (item.getItemId()) {
             case R.id.home:
                 getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
+                return true;
+
+            case R.id.create:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, createFragment).commit();
                 return true;
 
             case R.id.singlePlayer:
