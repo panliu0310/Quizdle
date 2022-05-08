fe21f9909b99e3acade6fa2d5e745d59e3044ace
panliu0310 <liuhoipan@gmail.com>
2022-05-08 11:46:33 +0800

    Add textView for CreateQuestionActivity

--- app/src/main/java/edu/cuhk/csci3310/quizdle/CreateQuestionActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/CreateQuestionActivity.java
@@ -3,10 +3,12 @@ package edu.cuhk.csci3310.quizdle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.TextView;
 
 import edu.cuhk.csci3310.quizdle.customview.DropDownMenuView;
 
@@ -14,8 +16,11 @@ public class CreateQuestionActivity extends AppCompatActivity {
 
     String TAG = "CreateQuestionActivity";
 
+    TextView tvGreeting;
     DropDownMenuView ddmvCategory;
     DropDownMenuView ddmvSubcategory;
+
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +29,14 @@ public class CreateQuestionActivity extends AppCompatActivity {
 
         setToolbar();
 
+        Intent intent = getIntent();
+        username = intent.getStringExtra("username");
+
+        tvGreeting = findViewById(R.id.tv_greeting);
         ddmvCategory = findViewById(R.id.ddmv_category);
         ddmvSubcategory = findViewById(R.id.ddmv_subcategory);
+
+        tvGreeting.setText(String.format(getString(R.string.create_question_greeting),username));
 
         setCategoryDropDownMenu();
 
@@ -33,6 +44,7 @@ public class CreateQuestionActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
                 Object item = parent.getItemAtPosition(pos);
                 Log.d(TAG, item.toString() + " category is picked");
+                // set subcategory drop down menu
                 ddmvSubcategory.setSpinner(CreateQuestionActivity.this, item.toString());
             }
             public void onNothingSelected(AdapterView<?> parent) {
