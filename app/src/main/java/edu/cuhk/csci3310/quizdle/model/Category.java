f33702212502db95a11ca5dbd207b08d3f113d55
hea0516 <hea4152@gmail.com>
2022-05-11 01:55:34 +0800

    Can connect to firebase but the recycler view still fixing

---
+++ app/src/main/java/edu/cuhk/csci3310/quizdle/model/Category.java
@@ -0,0 +1,13 @@
+package edu.cuhk.csci3310.quizdle.model;
+
+public class Category {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
